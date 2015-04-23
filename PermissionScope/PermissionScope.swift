301abe2b8fc7e668679178304f1eef17138d1c84
bre7 <emiliano_ber@hotmail.com>
2015-04-23 15:08:03 -0300

    UIColor extension => get inverseColor

--- PermissionScope/PermissionScope.swift
+++ PermissionScope/PermissionScope.swift
@@ -52,6 +52,14 @@ public struct PermissionResult: Printable {
         return "\(type.rawValue) \(status.rawValue)"
     }
 }
+
+extension UIColor {
+    var inverseColor: UIColor{
+        var r:CGFloat = 0.0; var g:CGFloat = 0.0; var b:CGFloat = 0.0; var a:CGFloat = 0.0;
+        if self.getRed(&r, green: &g, blue: &b, alpha: &a) {
+            return UIColor(red: 1.0-r, green: 1.0 - g, blue: 1.0 - b, alpha: a)
+        }
+        return self
     }
 }
 
