790043facea2b8e8c1b05d6dc7640de55671f242
Nick O'Neill <nick.oneill@gmail.com>
2015-05-26 15:36:41 -0700

    Remove the required warning... people can warn as much as they want.

--- PermissionScope/PermissionScope.swift
+++ PermissionScope/PermissionScope.swift
@@ -192,7 +192,6 @@ public class PermissionScope: UIViewController, CLLocationManagerDelegate, UIGes
         // close button
         closeButton.setTitle("Close", forState: UIControlState.Normal)
         closeButton.addTarget(self, action: Selector("cancel"), forControlEvents: UIControlEvents.TouchUpInside)
-        closeButton.setTranslatesAutoresizingMaskIntoConstraints(false)
         
         contentView.addSubview(closeButton)
     }
@@ -286,9 +285,6 @@ public class PermissionScope: UIViewController, CLLocationManagerDelegate, UIGes
         assert(!config.message.isEmpty, "Including a message about your permission usage is helpful")
         assert(configuredPermissions.count < 3, "Ask for three or fewer permissions at a time")
         assert(configuredPermissions.filter { $0.type == config.type }.isEmpty, "Permission for \(config.type.rawValue) already set")
-        if config.type == .Notifications && config.demands == .Required {
-            assertionFailure("We cannot tell if notifications have been denied so it's unwise to mark this as required")
-        }
 
         configuredPermissions.append(config)
     }
