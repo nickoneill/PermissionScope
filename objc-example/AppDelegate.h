45efa4cd2ddd73dded7a6853efe93a0f480a0dd8
Nick O'Neill <nick.oneill@gmail.com>
2015-06-26 13:01:02 -0700

    Added basic objc example project.

---
+++ objc-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  objc-example
+//
+//  Created by Nick O'Neill on 6/26/15.
+//  Copyright (c) 2015 That Thing in Swift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ objc-example/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  objc-example
+//
+//  Created by Nick O'Neill on 6/26/15.
+//  Copyright (c) 2015 That Thing in Swift. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
