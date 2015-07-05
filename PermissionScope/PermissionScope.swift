0959a256be92ffe69fadf8d43a324a964c13ccb3
Felix Dumit <felix.dumit@gmail.com>
2015-07-05 17:30:13 -0300

    support for nil callbacks

--- PermissionScope/PermissionScope.swift
+++ PermissionScope/PermissionScope.swift
@@ -736,8 +736,8 @@ extension String {
         assert(configuredPermissions.count > 0, "Please add at least one permission")
 
         // this is so it works with Objective-C too.
-        authChangeClosure = { (authChange)!(finished: $0, results: $1) }
-        cancelClosure = { (cancelled)!(results: $0) }
+        authChangeClosure = { (authChange)?(finished: $0, results: $1) }
+        cancelClosure = { (cancelled)?(results: $0) }
 
         // no missing required perms? callback and do nothing
         if requiredAuthorized {
