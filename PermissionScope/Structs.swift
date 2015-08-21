119c1b092565073e815b57ab4fc4437500685748
bre7 <mobileuy@gmail.com>
2015-08-21 19:05:58 -0300

    Closes #65

--- PermissionScope/Structs.swift
+++ PermissionScope/Structs.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-@objc public enum PermissionType: Int {
+@objc public enum PermissionType: Int, CustomStringConvertible {
     case Contacts, LocationAlways, LocationInUse, Notifications, Microphone, Camera, Photos, Reminders, Events, Bluetooth, Motion
     
     public var prettyDescription: String {
@@ -20,17 +20,49 @@ import Foundation
         }
     }
     
-    // Watch out for 
+    public var description: String {
+        switch self {
+        case .Contacts: return "Contacts"
+        case .Events: return "Events"
+        case .LocationAlways: return "LocationAlways"
+        case .LocationInUse: return "LocationInUse"
+        case .Notifications: return "Notifications"
+        case .Microphone: return "Microphone"
+        case .Camera: return "Camera"
+        case .Photos: return "Photos"
+        case .Reminders: return "Reminders"
+        case .Bluetooth: return "Bluetooth"
+        case .Motion: return "Motion"
+        }
+    }
+    
+    // Watch out for
     static let allValues = [Contacts, LocationAlways, LocationInUse, Notifications, Microphone, Camera, Photos, Reminders, Events, Bluetooth, Motion]
     
 }
 
-@objc public enum PermissionStatus: Int {
+@objc public enum PermissionStatus: Int, CustomStringConvertible {
     case Authorized, Unauthorized, Unknown, Disabled
+    
+    public var description: String {
+        switch self {
+        case .Authorized: return "Authorized"
+        case .Unauthorized:return "Unauthorized"
+        case .Unknown: return "Unknown"
+        case .Disabled: return "Disabled" // System-level
+        }
+    }
 }
 
-@objc public enum PermissionDemands: Int {
+@objc public enum PermissionDemands: Int, CustomStringConvertible {
     case Required, Optional
+    
+    public var description: String {
+        switch self {
+        case .Required: return "Required"
+        case .Optional: return "Optional"
+        }
+    }
 }
 
 @objc public class PermissionConfig: NSObject {
