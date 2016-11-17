8e2ee8ecd02ce5ea34b03e8b5bf8112c33f20295
Alan Scarpa <alan@intrepid.io>
2016-11-17 11:43:03 -0500

    Fix permissions

--- PermissionScope/Structs.swift
+++ PermissionScope/Structs.swift
@@ -10,9 +10,40 @@ import Foundation
 
 /// Permissions currently supportes by PermissionScope
 @objc public enum PermissionType: Int, CustomStringConvertible {
-    case contacts, locationAlways, locationInUse, notifications, microphone, camera, photos, reminders, events, bluetooth, motion
+    #if PermissionScopeRequestContactsEnabled
+    case contacts
+    #endif
+    #if PermissionScopeRequestLocationEnabled
+    case locationAlways
+    case locationInUse
+    #endif
+    #if PermissionScopeRequestNotificationsEnabled
+    case notifications
+    #endif
+    #if PermissionScopeRequestMicrophoneEnabled
+    case microphone
+    #endif
+    #if PermissionScopeRequestCameraEnabled
+    case camera
+    #endif
+    #if PermissionScopeRequestPhotoLibraryEnabled
+    case photos
+    #endif
+    #if PermissionScopeRequestRemindersEnabled
+    case reminders
+    #endif
+    #if PermissionScopeRequestEventsEnabled
+    case events
+    #endif
+    #if PermissionScopeRequestBluetoothEnabled
+    case bluetooth
+    #endif
+    #if PermissionScopeRequestMotionEnabled
+    case motion
+    #endif
     
     public var prettyDescription: String {
+        // TODO:  This will not compile due to same problem described below.
         switch self {
         case .locationAlways, .locationInUse:
             return "Location"
@@ -22,6 +53,8 @@ import Foundation
     }
     
     public var description: String {
+        /* TODO: This will not compile when used in a project (unless project is asking ALL permissions) because any permission that is not used will have it's enum undefined due to the #ifendif statements around the enum definition above.
+        */
         switch self {
         case .contacts:         return "Contacts"
         case .events:           return "Events"
@@ -37,7 +70,14 @@ import Foundation
         }
     }
     
-    static let allValues = [contacts, locationAlways, locationInUse, notifications, microphone, camera, photos, reminders, events, bluetooth, motion]
+    static var allValues: [PermissionType] {
+        var values = [PermissionType]()
+        for permission in iterateEnum(PermissionType.self) {
+            guard let permissionType = PermissionType(rawValue: permission.rawValue) else { continue }
+            values.append(permissionType)
+        }
+        return values
+    }
 }
 
 /// Possible statuses for a permission.
@@ -66,5 +106,18 @@ import Foundation
     
     override public var description: String {
         return "\(type) \(status)"
+    }
+}
+
+/* Used to iterate through available PermissionType based on permissions in use.  Taken from http://stackoverflow.com/a/28341290/3880396 */
+func iterateEnum<T: Hashable>(_: T.Type) -> AnyIterator<T> {
+    var i = 0
+    return AnyIterator {
+        let next = withUnsafePointer(to: &i) {
+            $0.withMemoryRebound(to: T.self, capacity: 1) { $0.pointee }
+        }
+        if next.hashValue != i { return nil }
+        i += 1
+        return next
     }
 }
